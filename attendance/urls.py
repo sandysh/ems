88f8983019a1eea14d5ba00017539c0fa818e9e9
Arpana05 <arpanaa905@gmail.com>
2024-08-02 23:10:26 +0545

    attendance wip

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("punch", views.punch, name="attendancePunch"),
     path("summary", views.summary, name="attendanceSummary"),
+    path('attendance_history/<int:user_id>/', views.attendance_history, name='attendance_history'),
 ]
