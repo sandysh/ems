95eb5b639318556cb784dfdaa34bf53e9e74a8fc
sandy <sandy@sandys-MacBook-Pro.local>
2024-11-06 17:32:23 +0545

    onesignal get user id phase 1

--- resources/js/kit.js
+++ resources/js/kit.js
@@ -61,23 +61,7 @@ export function destroy(url) {
 
 }
 
-export async function notify() {
-    // let response = await fetch('https://api.onesignal.com/notifications',{
-    //     method: "POST",
-    //     headers: {
-    //         "Authorization": 'Basic MDQ5M2E4MjktOGEwNC00Mzg0LTlkNjQtMDBiNzUwOTE5Zjdk',
-    //         "Access-Control-Allow-Origin": 'https://hrm.darkmattergame.net',
-    //         "Access-Control-Allow-Credentials": 'true',
-    //         "Accept": 'application/json',
-    //         "Content-Type":'application/json'
-    //     },
-    //     body: {
-    //         "app_id": "567b4ce3-247b-45cf-8999-a943c6a55897",
-    //         "target_channel": "push",
-    //         "contents": {"en": "English Message", "es": "Spanish Message"},
-    //         "included_segments": ["All"]
-    //     }
-    // })
+export async function notify(message) {
     axios.defaults.baseURL = 'https://api.onesignal.com/notifications';
     axios.defaults.headers.common['Authorization'] = 'Basic MDQ5M2E4MjktOGEwNC00Mzg0LTlkNjQtMDBiNzUwOTE5Zjdk';
     axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -87,7 +71,7 @@ export async function notify() {
     return axios.post('',{
       "app_id": "567b4ce3-247b-45cf-8999-a943c6a55897",
       "target_channel": "push",
-      "contents": {"en": "English Message", "es": "Spanish Message"},
+      "contents": {"en": message, "es": message},
       "included_segments": ["All"]
     })
 }
